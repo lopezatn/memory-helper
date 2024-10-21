c141ac4fe358944ac9a2a3f17b0e32783c38358e
lopezatn <agustinlpzb@gmail.com>
2024-10-21 14:22:50 +0200

    Bootstrap commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+
+    return (
+        <body>
+        <div className='login-navbar'>
+        </div>
+        </body>
+    )
+}
+
+export default Login;
