a284a805912506e8541e6b425b92b0ad62e91915
lopezatn <agustinlpzb@gmail.com>
2024-12-28 15:24:12 +0100

    Added an icon for finished reminders, visually more appealing

--- src/components/reminders/ReminderItem.jsx
+++ src/components/reminders/ReminderItem.jsx
@@ -2,19 +2,19 @@ import "./reminder-item.css";
 
 const ReminderItem = ({ reminder }) => {
   return (
-    <div
-      className={
-        reminder.finished
-          ? "bg-success list-group-item list-group-item-action active"
-          : "list-group-item list-group-item-action active"
-      }
-    >
-      <div className="title-date-desc-container">
+    <>
+      <div
+        className={
+          reminder.finished
+            ? "finished-icon padding-l title-date-desc-container"
+            : "title-date-desc-container"
+        }
+      >
         <small>{reminder.creationDate}</small>
         <h5 className="">{reminder.title}</h5>
       </div>
       <p className="title-date-desc-container">{reminder.description}</p>
-    </div>
+    </>
   );
 };
 
